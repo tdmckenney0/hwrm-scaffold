8dac9797f611f9dd27339ce9d21f827ed6cbab44
Tanner Mckenney <tmckenney7@outlook.com>
2023-03-09 15:31:41 -0800

    `Weapon` model test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
 fn main() {
-    println!("Hello, world!");
+    use self::schema::weapons::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = weapons
+        .limit(5)
+        .load::<models::weapon::Weapon>(connection)
+        .expect("Error loading weapons");
+
+    println!("Displaying {} weapons", results.len());
+    for weapon in results {
+        println!("{}", weapon.name);
+        println!("----------\n");
+        println!("{}", weapon.weapon_type);
+    }
 }
---
+++ src/models/weapon.rs
@@ -0,0 +1,32 @@
+use diesel::prelude::*;
+
+#[derive(Queryable)]
+pub struct Weapon {
+    pub id: i32,
+    pub name: String,
+    pub weapon_type: String,
+    pub weapon_fire_type: String,
+    pub weapon_fire_name: String,
+    pub activation: String,
+    pub fire_speed: f32,
+    pub fire_range: f32,
+    pub fire_radius: f32,
+    pub fire_lifetime: f32,
+    pub fire_anticipation_time: f32,
+    pub fire_axis: i32,
+    pub max_effects_spawned: i32,
+    pub lead_target: i32,
+    pub check_line_of_fire: i32,
+    pub fire_time: f32,
+    pub burst_fire_time: f32,
+    pub burst_wait_time: f32,
+    pub shoot_at_secondaries: i32,
+    pub shoot_at_surroundings: i32,
+    pub max_azimuth_speed: f32,
+    pub max_declination_speed: f32,
+    pub speed_multiplier: f32,
+    pub shield_penetration: String,
+    pub track_targets_outside_range: i32,
+    pub wait_for_code_red: f32,
+    pub instant_hit_threshold: i32,
+}
