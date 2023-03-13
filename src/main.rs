4f9cab4607af9c0af1b8ef5e5e72cb4dd37a7062
Tanner Mckenney <tmckenney7@outlook.com>
2023-03-12 20:42:12 -0700

    Import weapons directory

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,26 @@ use diesel::prelude::*;
 use diesel::insert_into;
 use dotenvy::dotenv;
 use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn find_files_with_extension(root_path: &PathBuf, extension: &str) -> Vec<PathBuf> {
+    let mut files = vec![];
+    if let Ok(entries) = fs::read_dir(root_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() && path.extension().unwrap_or_default() == extension {
+                    files.push(path);
+                } else if path.is_dir() {
+                    let mut sub_files = find_files_with_extension(&path, extension);
+                    files.append(&mut sub_files);
+                }
+            }
+        }
+    }
+    files
+}
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -18,8 +38,40 @@ fn main() {
     use crate::schema::weapons::dsl::*;
 
     let connection = &mut establish_connection();
+    let root_path = PathBuf::from(env::var("WEAPON_DIR").expect("WEAPON_DIR must be set"));
+    let extension = "wepn";
+    let files = find_files_with_extension(&root_path, extension);
+    let mut weapons_to_insert: Vec<models::weapon::StartWeaponConfig> = Vec::new();
+
+    // Delete all weapons first.
+    diesel::delete(weapons)
+        .execute(connection)
+        .expect("Error clearing weapons!");
+
+    for file in files {
+        let weapon_name = file.file_stem().unwrap().to_str().unwrap();
+
+        let contents = fs::read_to_string(&file)
+            .expect("Should have been able to read the file");
+
+        let lines = contents.lines();
+
+        for line in lines {
+            if line.starts_with("StartWeaponConfig") {
+                println!("parsing {}...", weapon_name);
+
+                weapons_to_insert.push(models::weapon::StartWeaponConfig::new(weapon_name.to_string(), line.to_string()));
+            }
+        }
+    }
+
+    insert_into(weapons)
+        .values(weapons_to_insert)
+        .execute(connection);
+
+    // List Weapons.
     let results = weapons
-        .limit(5)
+        .limit(10)
         .load::<models::weapon::Weapon>(connection)
         .expect("Error loading weapons");
 
@@ -28,27 +80,4 @@ fn main() {
     for weapon in &results {
         println!("{} -> {}\n", weapon.name, weapon);
     }
-
-    let results_wr = models::weapon::WeaponResult::belonging_to(&results)
-        .select(models::weapon::WeaponResult::as_select())
-        .load(connection)
-        .expect("Error loading weapon results");
-
-    println!("Displaying {} weapon results\n------------\n", results_wr.len());
-
-    for weapon_result in results_wr {
-        println!("{}", weapon_result);
-    }
-
-    let weapon_config_lua = "StartWeaponConfig(NewWeaponType,\"AnimatedTurret\",\"Bullet\",\"kinetic_rapid\",\"Normal\",1200,5000,0,0,0,0,1,1,1,0.1,2,5,1,1,120,120,0.1,\"Normal\",0,0,0)".to_string();
-    let start_weapon_config = models::weapon::StartWeaponConfig::new("hgn_dummyfrigategun3".to_string(), weapon_config_lua);
-
-    diesel::delete(weapons.filter(name.like(&start_weapon_config.name)))
-        .execute(connection)
-        .expect("Error deleting `hgn_dummyfrigategun3`");
-
-    insert_into(weapons)
-        .values(start_weapon_config)
-        .execute(connection)
-        .expect("Error inserting to weapons");
 }
